6f420183df2d36fd9b0d7bfb1da2d266d61eaf77
dongyudadiguo <3662728251@qq.com>
2025-03-31 16:56:35 +0800

    Update run.c

--- run.c
+++ run.c
@@ -1,15 +1,27 @@
+
+
 #include <stdlib.h>
+
+
 
 void (*imp)(), *bse, *crt;
 
+
+
 void function(void){
-    // ...
+    
 }
 
+
+
 int main(){
-    (*(void(**)(void))(bse = crt = malloc(sizeof(void (*)())*2))) = function;
-    // ...
+    imp = (*(void(**)(void))(bse = crt = malloc(sizeof(void (*)())))) = function;
+    
+    
+    
     while (1) imp();
 }
+
+
 
 // gcc *.c -o *.exe
