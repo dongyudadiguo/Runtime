9d0600d1c8154d472f41eeb183c87f922be16ff4
dongyudadiguo <3662728251@qq.com>
2025-04-26 11:30:48 +0800

    Update Runtime.c

--- Runtime.c
+++ Runtime.c
@@ -1,18 +1,13 @@
 void starter(void);
 
+int stack[];
+int stackPtr = 0;
+
 void *buffer;
 int ptr;
-void *std;
-void (*fun[])(void) = {starter};
+void (*fun[])(void) = {};
 void (*imp)(); 
 
-void starter(void)
-{
-    
-    imp = fun[(ptr = 0)];
-}
-
 int main(){
-    imp = fun[ptr = (*(int*)(buffer = malloc(sizeof(void *))) = 0)];
     while (1)imp();
 }
