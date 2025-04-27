34ee28f0d4339e8dc6ee730f749b896f680aac64
dongyudadiguo <3662728251@qq.com>
2025-04-27 11:26:23 +0800

    Update Runtime.c

--- Runtime.c
+++ Runtime.c
@@ -1,15 +1,17 @@
 
+void STK(void);
 int stack[];
 int stackPtr = 0;
-void STK(void){
-    stack[stackPtr++] = ptr;
-    imp = fun[*(int*)(buffer + (ptr = *(int*)(buffer + ptr + sizeof(int))))];
-}
 
 void *buffer;
 int ptr;
 void (*fun[])(void) = {STK};
 void (*imp)(); 
+
+void STK(void){
+    stack[stackPtr++] = ptr;
+    imp = fun[*(int*)(buffer + (ptr = *(int*)(buffer + ptr + sizeof(int))))];
+}
 
 int main(){
     while (1)imp();
